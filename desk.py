76d180ff909e9ed71ff59eea5a5245de96cd51c4
SkorodumovS <skorodumov.sergey25@gmail.com>
2020-01-15 20:36:17 +0300

    Update desk.py
    class function make_desk

--- desk.py
+++ desk.py
@@ -1,65 +1,54 @@
 import Tkinter as tk
 import tkFont as tkf
 
-SCALE = 2
+class Chess(object):
+	def __init__(self):
+		self.SCALE = 2
+		self.desk, self.labels = self.MakeDesk()
 
-desk = tk.Tk()
-l_label =[]
-frame_num = tk.Frame(desk)
-frame_num.pack(side = tk.LEFT, anchor=tk.NW)
-for i in range(1,9):
-	label = tk.Label(frame_num, text = i, bg = "green", width = SCALE * 2, height = SCALE)
-	label.pack(side = tk.BOTTOM)
+	def OnClick(self, event):
+		pass
 
+	def MakeDesk(self):
+		desk = tk.Tk()
+		frame_num = tk.Frame(desk)
+		frame_num.pack(side = tk.LEFT, anchor=tk.NW)
+		for i in range(1,9):
+			label = tk.Label(frame_num, text = i, bg = "green", width = self.SCALE * 2, height = self.SCALE)
+			label.pack(side = tk.BOTTOM)
 
-frame_let = tk.Frame(desk)
-frame_let.pack(side = tk.BOTTOM, anchor = tk.SE)
-for i in ["A", "B", "C", "D", "E", "F", "G", "H"]:
-	label = tk.Label(frame_let, text = i, bg = "red", width = SCALE * 2, height = SCALE)
-	label.pack(side = tk.LEFT)
+		frame_let = tk.Frame(desk)
+		frame_let.pack(side = tk.BOTTOM, anchor = tk.SE)
+		for i in ["A", "B", "C", "D", "E", "F", "G", "H"]:
+			label = tk.Label(frame_let, text = i, bg = "red", width = self.SCALE * 2, height = self.SCALE)
+			label.pack(side = tk.LEFT)
 
+		chess_frame = tk.Frame(desk)
+		chess_frame.pack()
 
+		labels = []
+		for i in range(8):
+			labels.append([])
+			for j in range(8):
+				if (j + i) % 2 != 0:
+					col = "black"
+				else:
+					col = "white"
+				label = tk.Label(chess_frame, bg = col, width = self.SCALE * 2, height = self.SCALE)
+				label.grid(row = i, column = j)
+				label.bind("<Button-1>", self.OnClick)
 
-chess_frame = tk.Frame(desk)
-chess_frame.pack()
-def do_smth(event):
-	pass
+				labels[i].append(label)
+				#label["text"] = "a"
 
-for i in range(8):
-	for j in range(8):
-		if (j + i) % 2 != 0:
-			col = "black"
-		else:
-			col = "white"
-		label = tk.Label(chess_frame, bg = col, width = SCALE * 2, height = SCALE)
+		return desk,labels
 
-
-		label.grid(row = i, column = j)
-		label.bind("<Button-1>", do_smth)
-
-		#label["text"] = "a"
-
-
-'''
-for i in range(8):
-	frame = tk.Frame(desk)
-	frame.pack()
-	for j in range(8):
-		label = tk.Label(frame, text = str(i*8+j), bg = 'white', fg="black")
-		label.pack(side = tk.LEFT)
-
-'''
+	def Run(self):
+		self.desk.mainloop()
+		print(1)
 
 
+if __name__ == "__main__":
+	board = Chess()
+	board.Run()
 
-
-
-#the_label = tk.Label(desk, text = "tre")
-#the_label.pack()
-#screen_height = desk.winfo_screenheight()
-#screen_width = desk.winfo_screenwidth()
-#print(screen_height)
-#print(screen_width)
-
-desk.mainloop()
-print(1)
